1e82e1db84ac5cc314e2ecf2ed03d24658897751
aman <saifiamaan445@gmail.com>
2026-02-23 22:20:20 +0530

    lexical scope and scope chaining

---
+++ class 10 (Lexical Scope and Closure)/lexicalScope.js
@@ -0,0 +1,22 @@
+//Q1. 
+// function parent(){
+//     console.log(a);
+//     var a = 10;
+//     function child(){
+//         console.log(a);
+//     }
+//     child();
+// }
+// parent();
+
+//Q2. 
+// var a = 20;
+// function parent(){
+//     console.log(a);
+//     var a = 10;
+//     function child(){
+//         console.log(a);
+//     }
+//     child();
+// }
+// parent();
---
+++ class 10 (Lexical Scope and Closure)/script.js
@@ -0,0 +1,7 @@
+console.log(a);
+var a = 10;
+
+test();
+function test(){
+    console.log("This is a test function");
+}

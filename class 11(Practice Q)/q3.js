bb42bbcd6f5785ace51e07a8ec8fce51ef1c8cb2
aman <saifiamaan445@gmail.com>
2026-02-25 22:06:43 +0530

    q4

--- class 11(Practice Q)/q3.js
+++ class 11(Practice Q)/q3.js
@@ -4,6 +4,7 @@ const students = [
   { name: "Alice", score: 92 },
   { name: "Bob", score: 80 },
   { name: "Charlie", score: 60 },
+  {name:"Aman",score:33}
 ];
 
 // Write a function that groups them into categories:
@@ -21,7 +22,31 @@ const students = [
 // Approach: Focus on the output expected. This has the clue to the approach
 
 function categorizeStudents(studentList) {
-  // Write your code here
+    const n = studentList.length;
+    const result = {
+        A:[],
+        B:[],
+        C:[]
+    };
+    for(let i=0;i<n;i++){
+        const student = studentList[i];
+        const name = student.name;
+        const marks = student.score;
+        if(marks >=90 && marks<=100){
+            //student will go into A
+            result.A.push(name);
+        }else if(marks>=75 && marks<90){
+            //student will go into B
+            result.B.push(name);
+        }else if(marks<75 && marks>0){
+            //student will go to C
+            result.C.push(name);
+        }else{
+            console.log("Marks are Incorrect")
+        }
+    }
+    console.log(result);
+    return result;
 }
 
 console.log(categorizeStudents(students));

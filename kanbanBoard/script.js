fdc4215cba873e0440095573fa247bda38d47595
aman <saifiamaan445@gmail.com>
2026-03-16 23:25:46 +0530

    dynamic ticket generation

--- kanbanBoard/script.js
+++ kanbanBoard/script.js
@@ -1,8 +1,10 @@
 const addBtn = document.querySelector(".add");
 const modal = document.querySelector('.modal');
 const mainEle = document.querySelector('main');
+const allPriorityColor = document.querySelectorAll('.priority-color');
 
 let isModalVisible = false;
+let newlyCreatedTaskPriority = 'red';
 
 addBtn.addEventListener('click',function(e){
     // console.log("Click happened on Plus icon");
@@ -15,18 +17,27 @@ addBtn.addEventListener('click',function(e){
     }
 })
 
+for(let i=0;i<allPriorityColor.length;i++){
+    allPriorityColor[i].addEventListener("click",function(e){
+        newlyCreatedTaskPriority = e.target.classList[1]
+        //remove active class from already selected;
+        for(let j=0;j<allPriorityColor.length;j++){
+            if(allPriorityColor[j].classList.contains('active'))
+                allPriorityColor[j].classList.remove('active');
+        }
+        e.target.classList.add('active');
+    })
+}
+
 modal.addEventListener('keydown',function(e){
     const key = e.key;
     if(key === 'Shift'){
-        createTicket();
+        createTicket(e.target.value,newlyCreatedTaskPriority);
         modal.style.display = 'none'; // once ticket is created hide the modal, it is UI
         isModalVisible = false; // update the state 
         e.target.value = "" // make the modal value as empty
     }
 })
-
-
-
 
 
 
@@ -38,14 +49,16 @@ modal.addEventListener('keydown',function(e){
     <div class="lock"><i class="fa fa-lock"></i></div>
 </div> 
 */}
-// createTicket();
+// createTicket("Learn JS","blue");
+// createTicket("Something else","pink");
 
-function createTicket(){
+function createTicket(taskName,priorityColour){
     const divEle = document.createElement('div');
     divEle.setAttribute('class','ticket');
-    divEle.innerHTML = `<div class="priority red"></div>
-                        <div class="ticketId">#fd454y</div>
-                        <div class="ticketContent">Task 1</div>
+    const uniqueId = crypto.randomUUID().split('-')[0];
+    divEle.innerHTML = `<div class="priority ${priorityColour}"></div>
+                        <div class="ticketId">#${uniqueId}</div>
+                        <div class="ticketContent">${taskName}</div>
                         <div class="lock">
                             <i class="fa fa-lock">
                         </i></div>`

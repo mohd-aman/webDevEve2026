bb68c9b19e87dc1685bd39ac2e5e273d2e5006bb
aman <saifiamaan445@gmail.com>
2026-04-10 22:01:16 +0530

    this keyword

---
+++ class 26 ( this keyword)/nodeNonStrict.js
@@ -0,0 +1,42 @@
+console.log("Scenario 1 : ");
+console.log(this); // {}
+
+console.log("Scenario 2 : ");
+
+function func(){
+    console.log(this); //global
+}
+
+func();
+
+
+const obj = {
+    name:"Mike",
+    fn:function (){
+        console.log(this);
+    }
+}
+console.log("Scenario 3 : ");
+obj.fn();
+
+console.log("Scenario 4 : ");
+const objFn = obj.fn;
+objFn();
+
+
+const obj2 = {
+  name:"Nested obj",
+  fn: function () {
+    console.log("Scenario 6 : ");
+    console.log(this);
+    const nestedFunc = function () {
+      console.log("Scenario 5 : ");
+      console.log(this);
+    };
+    nestedFunc();
+  },
+};
+
+obj2.fn();
+const obj2Fn = obj2.fn;
+obj2Fn();

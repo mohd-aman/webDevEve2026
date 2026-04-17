0c15f7a8b9cdbe7ac9ac6ad8676b5166d64cd510
aman <saifiamaan445@gmail.com>
2026-04-17 21:34:57 +0530

    call

---
+++ class29 (call,apply and bind)/call.js
@@ -0,0 +1,25 @@
+const person1 = {
+    name:"John",
+    age:25,
+    printNameAndAge:function(location,gender){
+        console.log(this);
+        console.log(`My name is ${this.name} and I am ${this.age} years old, and live in ${location}`)
+        console.log(gender);
+    }
+}
+const person2 = {
+    name:"Mark",
+    age:35,
+    location:"Delhi"
+}
+
+person1.printNameAndAge("Delhi","N/A"); //method invocation
+// person2.printNameAndAge();//? error as key does not exist
+
+
+// to borrow function
+person1.printNameAndAge.call(person2,"Delhi","Male");
+
+// func.call() // why - prototype of Function
+// arr.filter()//why - due to prototype of Array 
+// obj.hasOwnProperty()// why - due to prototype of Object

7e112b88c0527d51b8643906058194bcb4a63bdf
aman <saifiamaan445@gmail.com>
2026-04-26 15:24:51 +0530

    notes

--- class 32 (shallow vs deep)/deepCopy.js
+++ class 32 (shallow vs deep)/deepCopy.js
@@ -1,29 +1,27 @@
 let zoo = {
-    name:"Amazing Zoo",
-    location:"Melbourne, Australia",
-    animals:[
-        {
-            species:"Lion",
-            favoriteTreat:"Meat"
-        },
-        {
-            species:"Panda",
-            favoriteTreat:"Leaves"
-        }
-    ]
-}
+  name: "Amazing Zoo",
+  location: "Melbourne, Australia",
+  animals: [
+    {
+      species: "Lion",
+      favoriteTreat: "Meat",
+    },
+    {
+      species: "Panda",
+      favoriteTreat: "Leaves",
+    },
+  ],
+};
 
 //create a deep copy using JSON.stringify and JSON.parse;
 let zooString = JSON.stringify(zoo); // Primitive
-let deepCopyZoo = JSON.parse(zooString) //deep copy of zoo;
+let deepCopyZoo = JSON.parse(zooString); //deep copy of zoo;
 
 deepCopyZoo.location = "India";
-deepCopyZoo.animals[1]['species'] = "Horses";
-zoo.name = "New name"
+deepCopyZoo.animals[1]["species"] = "Horses";
+zoo.name = "New name";
 console.log(zoo);
 console.log(deepCopyZoo);
 
-
-console.log(zoo === deepCopyZoo);//?
-
-console.log(zoo.animals === deepCopyZoo.animals);//?
+console.log(zoo === deepCopyZoo); //?
+console.log(zoo.animals === deepCopyZoo.animals); //?
